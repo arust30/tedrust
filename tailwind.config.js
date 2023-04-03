ed80c5837fffc70a554494ae597b34321f3783e6
buckets <andrewrust30@gmail.com>
2023-04-02 23:34:53 -0400

    Updated Tailwind Files and Logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,59 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
 
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/flowbite-react/**/*.js",
+    "./**/*.html",
+  ],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.slate,
+      red: colors.rose,
+      pink: colors.fuchsia,
+      emerald: colors.emerald,
+      yellow: colors.amber,
+      green: colors.lime,
+      purple: colors.indigo,
+      orange: colors.orange,
+      blue: colors.blue,
+    },
+    screens: {
+      xs: "320px",
+      sm: "640px",
+      md: "768px",
+      lg: "1050px",
+      xl: "1280px",
+    },
+    fontFamily: {
+      sans: ["Montserrat", "Montserrat"],
+      serif: ["Montserrat", "Montserrat"],
+      mono: ["ui-monospace", "SFMono-Regular"],
+      extend: {
+        spacing: {
+          "2xl": "24rem",
+          "4xl": "48rem",
+          "6xl": "72rem",
+          "8xl": "96rem",
+          "10xl": "120rem",
+        },
+      },
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            enforce: "pre",
+            use: ["source-map-loader"],
+          },
+        ],
+      },
+      ignoreWarnings: [/Failed to parse source map/],
+    },
+  },
+};
