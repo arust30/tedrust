ecc12499072e3ae54495788461e4b4c826d64b87
buckets <andrewrust30@gmail.com>
2023-04-02 23:22:14 -0400

    Ted Rust Website initial commit!

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Family from "./pages/Family";
+import Sermons from "./pages/Sermons";
+import Biography from "./pages/Biography";
+import Contact from "./pages/Contact";
+import NavBar from "./pages/NavBar";
+import Footer from "./pages/Footer";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/family" element={<Family />} />
+          <Route path="/sermons" element={<Sermons />} />
+          <Route path="/biography" element={<Biography />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
