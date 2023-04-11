8e55ffce11ad6e87b129cc8196adea2d03ffa35a
buckets <andrewrust30@gmail.com>
2023-04-11 01:54:07 -0400

    updates to contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,55 +1,73 @@
 import React from "react";
 import TedPhone from "../assets/tedphone.png";
 import TedEmail from "../assets/tedemail.png";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    emailjs
+      .sendForm(
+        "service_h0bjfod",
+        "template_1ayf1yb",
+        event.target,
+        "ydP6hL_Ld_3jHHSlffKyi"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <div>
-      {/* <div className="h-[100vh]"> */}
-      <section class="bg-white dark:bg-gray-900">
-        <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
-          <div class="grid gap-16 lg:grid-cols-3">
-            <div class="hidden lg:block">
-              <h4 class="mb-1 pb-8 font-medium text-gray-900 dark:text-white">
+      <section className="bg-white dark:bg-gray-900">
+        <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+          <div className="grid gap-16 lg:grid-cols-3">
+            <div className="hidden lg:block">
+              <h4 className="mb-1 pb-8 font-medium text-gray-900 dark:text-white">
                 For questions, comments or inquiries about booking Pastor Rust
                 as a supply preacher:
               </h4>
               <img
-                className="h-10"
+                className="h-10 w-1/2"
                 src={TedPhone}
                 alt="Ted's phone number"
-                class="w-1/2"
               />
               <div className="pt-8"></div>
               <img
-                className="h-10"
+                className="h-10 w-1/2"
                 src={TedEmail}
                 alt="Ted's email address"
-                class="w-1/2"
               />
-              <h4 class="mb-1 font-medium text-gray-900 dark:text-white pt-8">
+              <h4 className="mb-1 font-medium text-gray-900 dark:text-white pt-8">
                 Or leave a comment in the comment box to the right
               </h4>
             </div>
-            <div class="col-span-2">
-              <h2 class="mb-4 text-3xl tracking-tight font-extrabold text-gray-900 lg:mb-8 md:text-4xl dark:text-white">
+            <div className="col-span-2">
+              <h2 className="mb-4 text-3xl tracking-tight font-extrabold text-gray-900 lg:mb-8 md:text-4xl dark:text-white">
                 Contact Ted
               </h2>
-              <form action="/" class="space-y-8">
+              <form onSubmit={handleSubmit} className="space-y-8">
                 <div>
                   <label
-                    for="email"
-                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                    htmlFor="email"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
                   >
                     Your email address{" "}
-                    <span class="text-xs text-gray-500">
+                    <span className="text-xs text-gray-500">
                       (So I can reply to you)
                     </span>
                   </label>
                   <input
                     type="email"
                     id="email"
-                    class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                    name="email"
+                    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
                     placeholder="name@email.com"
                     required
                   />
