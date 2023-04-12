e9c8243cf729fd85f2759359ca1d74af956c2e24
buckets <andrewrust30@gmail.com>
2023-04-12 13:26:23 -0400

    quick fix for contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -81,16 +81,16 @@ const Contact = () => {
                 </div>
                 <div>
                   <label
-                    for="subject"
+                    for="name"
                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
                   >
-                    Subject
+                    Name
                   </label>
                   <input
                     type="text"
-                    id="subject"
-                    name="subject"
-                    for="subject"
+                    id="name"
+                    name="name"
+                    for="name"
                     class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
                     placeholder="Let me know who you are"
                     required
